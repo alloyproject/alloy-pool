c731b2870f36dbd325bfe9d03183af8516812bd9
nighthawk18 <nighthawk18@protonmail.com>
2018-04-24 18:58:27 -0700

    first

---
+++ website/config.js
@@ -0,0 +1,18 @@
+var api = "http://ubuntu:8118";
+
+var poolHost = "ubuntu";
+
+var irc = "irc.freenode.net/#alloy";
+
+var email = "support@unbutu";
+
+var cryptonatorWidget = ["BTC-USD", "ETH-USD", "XMR-USD"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+
+
+var blockchainExplorer = "https://alloyexplorer.com/#/block/{id}";
+var transactionExplorer = "https://alloyexplorer.com/#/tx/{id}";
+
+var themeCss = "themes/ease-way-light-theme.css";
